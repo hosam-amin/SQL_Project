3e27d944c49f156ebae523fca49bf43c00ee496c
mo amin <mo8846489@gmail.com>
2025-08-12 03:13:23 +0300

    added readme and images

--- Project_SQL/3_Top_Demanded_skills.sql
+++ Project_SQL/3_Top_Demanded_skills.sql
@@ -11,7 +11,8 @@ SELECT
     skills,
     count(skills_job_dim.job_id) AS demand_count
 
-FROM job_postings_fact
+FROM 
+    job_postings_fact
      
 INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
 INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id 
